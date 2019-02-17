32c17507081a3662b7460058acfb39767d4c7ff0
haxiaocao <guokai2007@163.com>
2019-02-17 16:03:00 +0800

    add my custom tool bat

---
+++ open-this-directory.bat
@@ -0,0 +1,5 @@
+rem reference https://superuser.com/questions/181062/can-i-open-explorer-at-the-current-path-when-working-in-command-prompt
+rem start %windir%\explorer.exe .
+rem this custom tool can be named 'MY TOOL FOLDER'
+rem so I can use it for opening my CUSTOM TOOL FOLDER directly by clicking the TOOLS-> MY TOOL FOLDER
+start %windir%\explorer.exe "F:\SoftwareInstall\vs2017\enterprise\Common7\XiaocaiTools"
